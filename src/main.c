1c75c1ff3ca12e20de051e610599545afd61b484
jose5556 <josefilmeneses@gmail.com>
2025-03-01 14:45:23 +0000

    TODO

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 12:56:22 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/01 13:26:19 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/01 14:23:06 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@ int	main(void)
 
 	ft_bzero(&data, sizeof(t_data));
 	init(&data);
+	render_cub3d(&data);
+	//hooks_listener(); TODO
+	mlx_loop_hook(&data, render_cub3d, &data);
 	mlx_loop(data.mlx);
 }
