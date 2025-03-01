85f787e78aa497d0acf1ae6253074ec51a16b16b
jose5556 <josefilmeneses@gmail.com>
2025-03-01 13:27:12 +0000

    includes and init func

---
+++ includes/cub3d.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 12:56:36 by joseoliv          #+#    #+#             */
+/*   Updated: 2025/03/01 13:25:21 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libs/minilibx-linux/mlx.h"
+# include "../libs/libft/libft.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <unistd.h>
+# include <math.h>
+# include <stdio.h>
+
+# define HEIGHT	800
+# define WIDTH	800
+# define ERROR_MESSAGE "ERROR\n"
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_img;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*win;
+	t_img		img;
+}	t_data;
+
+//init
+void	init(t_data *data);
+
+#endif
