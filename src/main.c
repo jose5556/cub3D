fd969b1c47dc23cd1a3873956ac5632b8f65277c
jose5556 <josefilmeneses@gmail.com>
2025-03-03 18:01:22 +0000

    mlx_loop_hook corrected

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 12:56:22 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/01 14:23:06 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/03 17:54:20 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ int	main(void)
 {
 	t_data	data;
 
-	ft_bzero(&data, sizeof(t_data));
+	ft_memset(&data, 0, sizeof(t_data));
+	//map_validation();  TODO
 	init(&data);
 	render_cub3d(&data);
-	//hooks_listener(); TODO
-	mlx_loop_hook(&data, render_cub3d, &data);
+	hooks_listener(&data);
+	mlx_loop_hook(data.mlx, render_cub3d, &data);
 	mlx_loop(data.mlx);
 }
