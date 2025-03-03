1412fc9f43938bc552d92b7e6c26a0894db5e048
jose5556 <josefilmeneses@gmail.com>
2025-03-03 21:11:45 +0000

    pressed/released keys

--- src/events/hooks.c
+++ src/events/hooks.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 17:07:09 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/03 18:52:35 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/03 21:00:32 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,40 @@
 int	hooks_listener(t_game *game)
 {
 	mlx_hook(game->win, KeyPress, KeyPressMask,
-		handle_keys, game);
+		handle_keys_press, game);
+	mlx_hook(game->win, KeyRelease, KeyReleaseMask,
+		handle_keys_released, game);
 	mlx_hook(game->win, DestroyNotify, NoEventMask,
 		close_program, game);
 	return (0);
 }
 
-int	handle_keys(int keycode, t_game *game)
+int	handle_keys_press(int keycode, t_game *game)
 {
+	float	speed;
+
+	speed = 5;
 	if (keycode == XK_Escape)
 		close_program(game);
+	if (keycode == A)
+		game->player.key_left = true;
+	if (keycode == S)
+		game->player.key_down = true;
+	if (keycode == W)
+		game->player.key_up = true;
+	if (keycode == D)
+		game->player.key_right = true;
 	return (0);
+}
+
+int	handle_keys_released(int keycode, t_game *game)
+{
+	if (keycode == A)
+		game->player.key_left = false;
+	if (keycode == S)
+		game->player.key_down = false;
+	if (keycode == W)
+		game->player.key_up = false;
+	if (keycode == D)
+		game->player.key_right = false;
 }
