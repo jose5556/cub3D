99dce2447719d1d6cd7c64b1131773056641fa80
jose5556 <josefilmeneses@gmail.com>
2025-03-04 18:10:12 +0000

    error funcs

--- src/exit/exit_error_message.c
+++ src/exit/exit_error_message.c
@@ -6,14 +6,31 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 15:09:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/04 15:14:00 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:01:07 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int	exit_error(char *error_message)
+int	simple_exit_error(char *error_message)
 {
-	printf("%s%s\n", STANDART_ERROR ,error_message);
+	printf("%s%s\n", STANDART_ERROR, error_message);
 	return (0);
+}
+
+void	mlx_exit_error(char *error_message, t_game *game, int error)
+{
+	if (error == 1)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+	else if (error == 2)
+	{
+		mlx_destroy_display(game->mlx);
+		mlx_destroy_window(game->mlx, game->win);
+		free(game->mlx);
+	}
+	printf("%s%s\n", STANDART_ERROR, error_message);
+	exit(EXIT_FAILURE);
 }
--- src/exit/hoocks_exit.c
+++ src/exit/hoocks_exit.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 17:13:24 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/04 15:08:30 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:08:36 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	close_program_hook(t_game *game)
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
 	free(game->mlx);
+	free(game->map);
 	exit (EXIT_SUCCESS);
 	return (0);
 }
