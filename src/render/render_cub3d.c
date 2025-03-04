7e5dbb36a8498aea021bddef981b75502c0d3e08
jose5556 <josefilmeneses@gmail.com>
2025-03-04 20:28:04 +0000

    renders lines (ray casting)

--- src/render/render_cub3d.c
+++ src/render/render_cub3d.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 14:18:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/04 17:41:19 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/04 19:07:02 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	draw_player(int size, int color, t_game *game)
 		my_mlx_pixel_put(&game->img, game->player.x + i, game->player.y + size, color);  //left
 }
 
-static void	clean_image(t_game *game)
+static void	clear_image(t_game *game)
 {
 	int	y;
 	int	x;
@@ -44,14 +44,57 @@ static void	clean_image(t_game *game)
 	}
 }
 
+static bool	touch(float px, float py, t_game *game)
+{
+	int x;
+	int y;
+
+	x = px / SIZE;
+	y = py / SIZE;
+	if(game->map[y][x] == '1')
+		return true;
+	return false;
+}
+
+static void	draw_line(t_game *game, float start_x, int i)
+{
+	float cos_angle;
+	float sin_angle;
+	float ray_x;
+	float ray_y;
+
+	cos_angle = cos(start_x);
+	sin_angle = sin(start_x);
+	ray_x = game->player.x;
+	ray_y = game->player.y;
+	while(!touch(ray_x, ray_y, game))
+	{
+		my_mlx_pixel_put(&game->img, ray_x, ray_y, RED);
+		ray_x += cos_angle;
+		ray_y += sin_angle;
+	}
+}
+
 int	render_cub3d(void *param)
 {
 	t_game	*game;
+	int		i;
+	float	fraction;
+	float	start_x;
 
 	game = (t_game *)param;
 	move_player(&game->player);
-	clean_image(game);
+	clear_image(game);
 	draw_player(10 ,GREEN, game);
 	draw_map(game);
+	fraction = PI / 3 / WIDTH;
+	start_x = game->player.angle - PI / 6;
+	i = -1;
+	while(++i < WIDTH)
+	{
+		draw_line(game, start_x, i);
+		start_x += fraction;
+		i++;
+	}
 	mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
 }
