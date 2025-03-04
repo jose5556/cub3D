92146f361f42ea7977fb547faa8a3fb5f5155e85
jose5556 <josefilmeneses@gmail.com>
2025-03-04 15:58:59 +0000

    angle variantes

--- src/events/player_movement.c
+++ src/events/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 20:43:56 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/03 20:59:48 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:51:49 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,43 @@
 
 void	move_player(t_player *player)
 {
-	int	speed;
+	int		speed;
+	float	angle_speed;
+	float	cos_angle;
+	float	sin_angle;
 
-	speed = 7;
-	if (player->key_left)
-		player->x -= speed;
-	if (player->key_right)
-		player->x += speed;
+	speed = 5;
+	angle_speed = 0.03;
+	cos_angle = cos(player->angle);
+	sin_angle = sin(player->angle);
+
+	if (player->left_direction)
+		player->angle -= angle_speed;
+	if (player->right_direction)
+		player->angle += angle_speed;
+	if (player->angle > 2 * PI)
+		player->angle = 0;
+	if (player->angle < 0)
+		player->angle = 2 * PI;
+	
 	if (player->key_up)
-		player->y -= speed;
+	{
+		player->x -= cos_angle * speed;
+		player->y -= sin_angle * speed;
+	}
 	if (player->key_down)
-		player->y += speed;
+	{
+		player->x += cos_angle * speed;
+		player->y += sin_angle * speed;
+	}
+	if (player->key_left)
+	{
+		player->x -= sin_angle * speed;
+		player->y += cos_angle * speed;
+	}
+	if (player->key_right)
+	{
+		player->x += sin_angle * speed;
+		player->y -= cos_angle * speed;
+	}
 }
