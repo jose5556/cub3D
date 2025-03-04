236e303fc690d827cd8471bb19df1e4d63dfcfeb
jose5556 <josefilmeneses@gmail.com>
2025-03-04 20:28:13 +0000

    comment

--- src/render/render_map.c
+++ src/render/render_map.c
@@ -6,12 +6,25 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:34:55 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/03/04 18:06:56 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/03/04 20:24:47 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+/* void	draw_square_map(int x, int y, int size, int color, t_game *game)  //temp
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (++i < size)
+	{
+		j = -1;
+		while (++j < size)
+			my_mlx_pixel_put(&game->img, x + j, y + i, color);
+	}
+} */
 void	draw_square_map(int x, int y, int size, int color, t_game *game)  //temp
 {
 	int	i;
@@ -54,14 +67,14 @@ void	draw_map(t_game *game)  //temp
 char **get_map(void)   //temp func
 {
     char **map;
-	
+
 	map = malloc(sizeof(char *) * 11);
     map[0] = "111111111111111";
     map[1] = "100000000000001";
     map[2] = "100000000000001";
     map[3] = "100000100000001";
     map[4] = "100000000000001";
-    map[5] = "100000010000001";
+    map[5] = "100000010N00001";
     map[6] = "100001000000001";
     map[7] = "100000000000001";
     map[8] = "100000000000001";
